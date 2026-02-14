4ebcb975643ec94f10e1c500b19c58b936b91443
Cumi <mv0288820@gmail.com>
2026-02-14 12:00:00 +0300

    добавлены модели Category и Breed с миграциями

---
+++ database/migrations/2026_03_03_061937_create_breeds_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('breeds', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name', 100);
+            $table->boolean('is_verified')->default(false);
+            $table->timestamps();
+            
+            // Индексы
+            $table->index('category_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('breeds');
+    }
+};
